e5a4624e015175dbef5468e06c692a89f38fbf2a
unknown <unknown@unknown.(none)>
2009-11-07 11:30:53 +0200

    Another update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   belongs_to :coverage
   belongs_to :coverage_period
-  has_many :users, :through=>:contact
+  has_many :contacts
+  has_many :users, :through => :contact
   accepts_nested_attributes_for :coverage, :coverage_period
 
   def updatecontacts(contact_ids)
