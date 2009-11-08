eeed9e6d128e4df84d298f6294052837f9973aaf
unknown <unknown@unknown.(none)>
2009-11-08 17:39:19 +0200

    update contacts with limit complete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
 
   def edit
     @user = User.find(params[:id])  
-    @all_contacts = User.find(:all)
+    @all_contacts = User.paginate :page => params[:page], :per_page => 10
   end
 
   def destroy
