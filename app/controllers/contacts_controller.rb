fe34a6b15d3f31ead02c07987bab6e56ff4c80c3
unknown <unknown@unknown.(none)>
2009-11-08 19:24:44 +0200

    page2 now loads selected contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,8 @@
 class ContactsController < ApplicationController
 
   def edit
-    @user = User.find(params[:id])  
+    @user = User.find(params[:id])
+    @set_contacts = Contact.connection.select_values('select contact_id from contacts where user_id  = '+@user.id.to_s)    
     @all_contacts = User.paginate :page => params[:page], :per_page => 10
   end
 
--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,10 @@
 module ContactHelper
+  def check_selected_contacts(cid)
+     @set_contacts.each do |x|
+     if x.to_s == cid.to_s
+       return 'checked'
+     end
+    end
+     return nil
+  end
 end
