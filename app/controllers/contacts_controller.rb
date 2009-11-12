33d63e4daf283cd077711e0f4ee626b610398050
unknown <unknown@unknown.(none)>
2009-11-12 11:40:08 +0200

    Fixed bug when users selectable contacts are under 3 and can't then submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,11 @@ class ContactsController < ApplicationController
   def edit
     @user = User.find(params[:id])  
     @all_contacts = User.paginate :page => params[:page], :per_page => 5
+    @contact_count = 0
+    unless User.find(:all).count >= 3
+      @contact_count = 3
+    end           
+
   end
 
   def destroy
