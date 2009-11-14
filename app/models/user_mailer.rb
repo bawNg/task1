eb682549bcb80d0350ec61a6e020defb125804db
unknown <unknown@unknown.(none)>
2009-11-14 14:55:51 +0200

    fixed login

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
     from        "test@task1.defirence.za.net"
     subject     "Thank you for registering"
     body        :user => user
+    content_type "text/html"
   end
 
   def contact_information(recipient, sender, message)
@@ -11,6 +12,8 @@ class UserMailer < ActionMailer::Base
     from        "test@task1.defirence.za.net"
     subject     "Contact information for #{sender.first_name} #{sender.last_name}"
     body        :recipient => recipient, :sender => sender, :message => message
+    content_type "text/html"
+    
   end
 
 end
