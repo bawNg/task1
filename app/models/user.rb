b5bcb1c1941221643dcdafe63ed1160538545548
unknown <unknown@unknown.(none)>
2009-11-17 15:10:52 +0200

    Cosmetics and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,23 @@ class User < ActiveRecord::Base
   has_many :contactships
   has_many :contacts, :through => :contactships
   accepts_nested_attributes_for :coverage, :coverage_period
-  validates_presence_of :first_name, :last_name, :address, :city, :contact_number, :email_address, :state, :coverage_period, :coverage, :date_of_birth, :postal_code, :height, :weight
+  validates_presence_of :first_name, :last_name, :address,
+                        :city, :contact_number, :email_address,
+                        :state, :coverage_period, :coverage,
+                        :date_of_birth, :postal_code, :height,
+                        :weight
   validates_format_of :contact_number,
-                      :with => /^(\+[0-9]{0,4}\s)?[0-9\s]*$/,
-                      :message => "Invalid, can contain +code xxxxxxx OR xxxxxxx"
+                      :with => /^[0-9]{3}\s[0-9]{3}\s[0-9]{4}$/,
+                      :message => "Invalid format, xxx xxx xxxx, 10 digits only"
+  validates_format_of :weight,
+                      :with => /^[0-9]{1,3}\.0$/,
+                      :message => "3 digits only"
+  validates_format_of :height,
+                      :with => /^[0-9]{1,4}\.0$/,
+                      :message => "4 digits only" 
+  validates_format_of :postal_code,
+                      :with => /^[0-9]{6}$/,
+                      :message => "6 digits only"
   validates_format_of :email_address,
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/,
                       :message => "appears not to be valid"
